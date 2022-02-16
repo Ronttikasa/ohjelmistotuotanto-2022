f87a2bc01bad0f5986a93517189b82b36a9d6bc9
Silja Ojanen <44512829+Ronttikasa@users.noreply.github.com>
2022-02-16 22:07:20 +0200

    pylint säännöt rikottu

--- src/varasto.py
+++ src/varasto.py
@@ -5,7 +5,6 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -31,15 +30,41 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
+            if self.saldo == 0:
+                if self.tilavuus > 100:
+                    return -60
+            return kaikki_mita_voidaan
 
+        self.saldo = self.saldo - maara
+        if self.saldo > 10:
+            print("Too many statements (%s/%s) Used when a function or method has too many statements. You should then split it in smaller functions / methods.")
+        print("rikotaan")
+        print("pylint")
+        print("sääntöjä")
+        print("niin")
+        print("ahkerasti")
+        print("ettei")
+        print("mitään")
+        print("rajaa")
+        print("koko")
+        print("touhussa")
+
+        if maara < 0:
+            return 0.0
+        if maara > self.saldo:
+            kaikki_mita_voidaan = self.saldo
+            self.saldo = 0.0
+            if self.saldo == 0:
+                if self.tilavuus > 100:
+                    return -60
             return kaikki_mita_voidaan
 
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
